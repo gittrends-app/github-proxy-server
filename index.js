7dc1d49c81d583c5fb94cab98c03adb78ba47c28
Hudson Silva Borges <hudsonsilbor@gmail.com>
2020-04-12 01:45:33 -0400

    feat: first version

---
+++ index.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+
+/* Author: Hudson S. Borges */
+const fs = require('fs');
+const path = require('path');
+const helmet = require('helmet');
+const express = require('express');
+const compression = require('compression');
+const cliProgress = require('cli-progress');
+
+const { uniq, pick } = require('lodash');
+const { program } = require('commander');
+
+const debug = require('debug')('github-proxy');
+const loadBalancer = require('./load-balancer.js');
+
+// function to parse tokens from the input
+const tokensParser = (string) =>
+  string
+    .trim()
+    .replace(/\s+/gi, ' ')
+    .split(/[\s]/i)
+    .map((v) => v.replace(/.*:(.+)/i, '$1'));
+
+// function to concat tokens in commander
+const concatTokens = (token, list) => {
+  if (token.length !== 40)
+    throw new Error('Github access tokens have 40 characters');
+  return uniq([...list, token]);
+};
+
+// function to read tokens from a file
+const getTokens = (filename) => {
+  const filepath = path.resolve(__dirname, filename);
+  if (!fs.existsSync(filepath)) throw new Error(`File "${filename}" not found!`);
+  const tokens = tokensParser(fs.readFileSync(filepath, 'utf8'));
+  return tokens.reduce((acc, token) => concatTokens(token, acc), []);
+};
+
+// parse arguments from command line
+program
+  .option('-p, --port <port>', 'Port to start the proxy server', 3000)
+  .option('-t, --token <token>', 'GitHub token to be used', concatTokens, [])
+  .option('--tokens <file>', 'File containing a list of tokens', getTokens)
+  .option('--request-interval <interval>', 'Interval between requests (ms)', 100)
+  .option('--request-timeout <timeout>', 'Request timeout (ms)', 15000)
+  .option('--min-remaining <number>', 'Stop using token on', 100)
+  .parse(process.argv);
+
+if (!program.token.length && !(program.tokens && program.tokens.length)) {
+  process.stderr.write(`${program.helpInformation()}\n`);
+  process.stderr.write(`Arguments missing (see "--token" and "--tokens").\n\n`);
+  process.exit(1);
+}
+
+// create the load balancer
+const tokens = [...program.token, ...(program.tokens || [])];
+const options = pick(program, ['requestInterval', 'requestTimeout', 'minRemaining']);
+const balancer = loadBalancer(tokens, options);
+
+// create progress bars
+if (!debug.enabled && process.stderr.isTTY) {
+  const multibar = new cliProgress.MultiBar({
+    format: '{version} |{bar}| {value} remaining | {queued} queued',
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    clearOnComplete: false,
+    hideCursor: true,
+    formatValue: (v) =>
+      `${v}`.padStart(Math.floor(Math.log10(balancer.clients.length * 5000)))
+  });
+
+  ['rest', 'graphql'].forEach((api) => {
+    const bar = multibar.create();
+    bar.start(0, 0, { version: api.padStart(7), queued: 0 });
+
+    setInterval(() => {
+      const { clients } = balancer;
+      const rem = clients.reduce((a, c) => a + c[api].remaining, 0);
+      const queued = clients.reduce((a, c) => a + c[api].queued(), 0);
+      bar.update(rem, { queued });
+      bar.setTotal(clients.reduce((a, c) => a + c[api].limit, 0));
+    }, 500);
+  });
+}
+
+// start proxy server
+const app = express();
+
+app.use(compression());
+app.use(helmet());
+app.use(express.json());
+
+app.post('/graphql', balancer.graphql);
+app.get('/*', balancer.rest);
+
+app.all('/*', (req, res) =>
+  res
+    .status(501)
+    .json({ message: 'Only GET requests are supported by the proxy server' })
+);
+
+app.listen(program.port, () => {
+  process.stdout.write(
+    `Proxy server running on ${program.port} (tokens: ${tokens.length})\n`
+  );
+});
---
+++ load-balancer.js
@@ -0,0 +1,135 @@
+/* Author: Hudson S. Borges */
+const Bottleneck = require('bottleneck');
+const debug = require('debug')('github-proxy');
+
+const { chain, omit, each, cloneDeep } = require('lodash');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const fakeReset = () => Math.floor(Date.now() / 1000) + 60 * 60;
+
+module.exports = (
+  tokens = [],
+  { requestInterval = 100, requestTimeout = 15000, minRemaining = 100 } = {}
+) => {
+  // prepare clients
+  const clients = tokens.map((token) => {
+    const shortToken = token && token.substring(0, 4);
+
+    const metadata = {
+      rest: {
+        remaining: 5000,
+        reset: fakeReset(),
+        bottleneck: new Bottleneck({ maxConcurrent: 1, minTime: requestInterval })
+      },
+      graphql: {
+        remaining: 5000,
+        reset: fakeReset(),
+        bottleneck: new Bottleneck({ maxConcurrent: 1, minTime: requestInterval })
+      }
+    };
+
+    setInterval(() => {
+      each(metadata, (value) => {
+        if (value.reset && Math.floor(Date.now() / 1000) > value.reset) {
+          debug(`Rate limit reseted for ${shortToken}`);
+          value.remaining = 5000;
+          value.reset = fakeReset();
+        }
+      });
+    }, 5000);
+
+    const updateLimits = (version, headers) => {
+      metadata[version].remaining = parseInt(headers['x-ratelimit-remaining'], 10);
+      metadata[version].limit = parseInt(headers['x-ratelimit-limit'], 10);
+      metadata[version].reset = parseInt(headers['x-ratelimit-reset'], 10);
+    };
+
+    const log = (version, status, startedAt) => {
+      if (debug.enabled)
+        debug('%o', {
+          version,
+          token: shortToken,
+          jobs: metadata[version].bottleneck.jobs().length,
+          rateLimit: {
+            remaining: metadata[version].remaining,
+            reset: new Date(metadata[version].reset * 1000)
+          },
+          status,
+          duration: `${(Date.now() - startedAt) / 1000}s`
+        });
+    };
+
+    const apiProxy = createProxyMiddleware({
+      target: 'https://api.github.com',
+      changeOrigin: true,
+      headers: { authorization: `token ${token}` },
+      proxyTimeout: requestTimeout,
+      followRedirects: true,
+      logLevel: 'silent',
+      onProxyReq: (proxyReq, req) => {
+        req.headers.date = new Date();
+        if (req.method === 'GET' && /^\/user\/?$/i.test(req.originalUrl))
+          proxyReq.removeHeader('authorization');
+        if (req.method === 'POST' && /^\/graphql\/?$/i.test(req.originalUrl)) {
+          if (/viewer(.|\s)*{(.|\s)+}/i.test(req.body.query))
+            proxyReq.removeHeader('authorization');
+          const bodyData = JSON.stringify(req.body);
+          proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+          proxyReq.write(bodyData);
+        }
+      },
+      onProxyRes: (proxyRes, req) => {
+        const version = req.path === '/graphql' ? 'graphql' : 'rest';
+        updateLimits(version, proxyRes.headers);
+        log(version, proxyRes.statusCode, req.headers.date);
+        Object.assign(proxyRes, {
+          headers: omit(proxyRes.headers, [
+            'x-ratelimit-limit',
+            'x-ratelimit-remaining',
+            'x-ratelimit-reset',
+            'x-oauth-scopes',
+            'x-oauth-client-id'
+          ])
+        });
+      }
+    });
+
+    each(metadata, (value) => {
+      const { bottleneck } = value;
+      value.schedule = (...args) => bottleneck.schedule(() => apiProxy(...args));
+      value.jobs = () => bottleneck.jobs().length;
+      value.queued = () => bottleneck.queued();
+    });
+
+    return metadata;
+  });
+
+  // function to select the best client and queue request
+  function balancer(version, req, res, next) {
+    const client = chain(clients)
+      .filter((c) => c[version].remaining - c[version].jobs() > minRemaining)
+      .shuffle()
+      .first()
+      .value();
+
+    if (!client)
+      return res.status(503).json({
+        message: 'Proxy Server: no requests available',
+        reset: chain(clients)
+          .minBy((c) => c[version].reset)
+          .get([version, 'reset'])
+          .value()
+      });
+
+    return client[version].schedule(req, res, next);
+  }
+
+  // express config
+  return {
+    get clients() {
+      return clients.map((c) => cloneDeep(omit(c, ['bottleneck', 'schedule'])));
+    },
+    graphql: (...args) => balancer('graphql', ...args),
+    rest: (...args) => balancer('rest', ...args)
+  };
+};
