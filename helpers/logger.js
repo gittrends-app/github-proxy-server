f5fb8ae9735ee234b672bb759c2316b52c6af9fd
Hudson S. Borges <hudsonsilbor@gmail.com>
2020-11-30 16:35:16 -0400

    feat: add new logging and clear unnecessary code

---
+++ helpers/logger.js
@@ -0,0 +1,45 @@
+/* Author: Hudson S. Borges */
+const chalk = require('chalk');
+const dayjs = require('dayjs');
+const relativeTime = require('dayjs/plugin/relativeTime');
+const { createStream, getBorderCharacters } = require('table');
+
+dayjs.extend(relativeTime);
+
+const stream = createStream({
+  columnCount: 7,
+  columnDefault: { alignment: 'right', width: 10 },
+  columns: {
+    0: { width: 8 },
+    1: { width: 6 },
+    2: { width: 6 },
+    4: { width: 15 },
+    5: { width: 6 }
+  },
+  drawHorizontalLine: (index) => index === 0,
+  singleLine: true,
+  border: getBorderCharacters('void')
+});
+
+let count = -1;
+
+module.exports = ({ api, token, queued, remaining, reset, status, duration }) => {
+  // eslint-disable-next-line no-cond-assign
+  if (!(count = (count + 1) % 25)) {
+    stream.write(
+      ['api', 'token', 'queued', 'remaining', 'reset', 'status', 'duration'].map((v) =>
+        chalk.bold(v)
+      )
+    );
+  }
+
+  stream.write([
+    api,
+    token,
+    queued,
+    remaining,
+    dayjs.unix(reset).fromNow(),
+    chalk[/[45]\d{2}/i.test(status) ? 'redBright' : 'green'](status),
+    `${duration / 1000}s`
+  ]);
+};
--- index.js
+++ index.js
@@ -2,22 +2,20 @@
 global.Promise = require('bluebird');
 
 /* Author: Hudson S. Borges */
-const fs = require('fs');
-const path = require('path');
 const https = require('https');
 const polka = require('polka');
 const consola = require('consola');
 const send = require('@polka/send-type');
 const bodyParser = require('body-parser');
 const compression = require('compression');
-const cliProgress = require('cli-progress');
 
-const { uniq, pick, compact } = require('lodash');
+const { resolve } = require('path');
 const { program } = require('commander');
+const { uniq, pick, compact } = require('lodash');
+const { existsSync, readFileSync } = require('fs');
 
 const { version } = require('./package.json');
 const middleware = require('./middleware.js');
-const formatter = require('./helpers/formatter');
 
 // function to parse tokens from the input
 const tokensParser = (string) =>
@@ -35,9 +33,9 @@ const concatTokens = (token, list) => {
 
 // function to read tokens from a file
 const getTokens = (filename) => {
-  const filepath = path.resolve(process.cwd(), filename);
-  if (!fs.existsSync(filepath)) throw new Error(`File "${filename}" not found!`);
-  const tokens = tokensParser(fs.readFileSync(filepath, 'utf8'));
+  const filepath = resolve(process.cwd(), filename);
+  if (!existsSync(filepath)) throw new Error(`File "${filename}" not found!`);
+  const tokens = tokensParser(readFileSync(filepath, 'utf8'));
   return tokens.reduce((acc, token) => concatTokens(token, acc), []);
 };
 
@@ -49,9 +47,8 @@ program
   .option('--request-interval <interval>', 'Interval between requests (ms)', Number, 100)
   .option('--request-timeout <timeout>', 'Request timeout (ms)', Number, 15000)
   .option('--min-remaining <number>', 'Stop using token on', Number, 100)
-  .option('--verbose', 'Enable verbose mode')
   .version(version, '-v, --version', 'output the current version')
-  .parse(process.argv);
+  .parse();
 
 if (!program.token.length && !(program.tokens && program.tokens.length)) {
   consola.info(`${program.helpInformation()}`);
@@ -65,7 +62,7 @@ if (!program.token.length && !(program.tokens && program.tokens.length)) {
     await Promise.all(
       [...program.token, ...(program.tokens || [])].map(
         (token) =>
-          new Promise((resolve) =>
+          new Promise((r) =>
             https.get(
               'https://api.github.com/user',
               {
@@ -74,7 +71,7 @@ if (!program.token.length && !(program.tokens && program.tokens.length)) {
                   'user-agent': 'GitHub API Proxy Server (@hsborges/github-proxy-server)'
                 }
               },
-              ({ statusCode }) => resolve(statusCode === 200 ? token : null)
+              ({ statusCode }) => r(statusCode === 200 ? token : null)
             )
           )
       )
@@ -90,32 +87,6 @@ if (!program.token.length && !(program.tokens && program.tokens.length)) {
 
   const balancer = middleware(tokens, options);
 
-  // create progress bars
-  if (!program.verbose && process.stderr.isTTY) {
-    const multibar = new cliProgress.MultiBar({
-      format: '{version} |{bar}| {value} remaining | {queued} queued',
-      barCompleteChar: '\u2588',
-      barIncompleteChar: '\u2591',
-      clearOnComplete: false,
-      hideCursor: true,
-      formatValue: (v) =>
-        `${v}`.padStart(Math.floor(Math.log10(balancer.clients.length * 5000)))
-    });
-
-    ['rest', 'graphql'].forEach((api) => {
-      const bar = multibar.create();
-      bar.start(0, 0, { version: api.padStart(7), queued: 0 });
-
-      setInterval(() => {
-        const { clients } = balancer;
-        const rem = clients.reduce((a, c) => a + c[api].remaining, 0);
-        const queued = clients.reduce((a, c) => a + c[api].queued(), 0);
-        bar.update(rem, { queued });
-        bar.setTotal(clients.reduce((a, c) => a + c[api].limit, 0));
-      }, 500);
-    });
-  }
-
   // start proxy server
   const app = polka();
 
@@ -130,7 +101,12 @@ if (!program.token.length && !(program.tokens && program.tokens.length)) {
 
   app.listen(program.port, () => {
     consola.success(`Proxy server running on ${program.port} (tokens: ${tokens.length})`);
-    consola.success(`Options: ${formatter.object(options)}`);
+    consola.success(
+      `Options: %s`,
+      Object.keys(options)
+        .map((k) => `${k}: ${options[k]}`)
+        .join(', ')
+    );
   });
 
   process.on('SIGTERM', async () => {
