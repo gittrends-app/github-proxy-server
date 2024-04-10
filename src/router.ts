bfba245405f57d9ad65957b94963d7ba7217b141
Hudson S. Borges <hudsonsilbor@gmail.com>
2024-04-10 18:06:43 -0300

    chore: disable lint warning

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-duplicate-enum-values */
+
 /* Author: Hudson S. Borges */
 import Bottleneck from 'bottleneck';
 import { Request, Response } from 'express';
