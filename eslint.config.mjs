779eef6736c9eea8f64b9095b61538e592d103b2
Hudson S. Borges <hudsonsilbor@gmail.com>
2024-07-06 23:53:19 -0300

    chore: apply eslint config migrations

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const compat = new FlatCompat({
+  baseDirectory: path.dirname(fileURLToPath(import.meta.url)),
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    ignores: ['**/node_modules', '**/dist', '**/tsconfig.json', 'samples/**/*']
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+      },
+
+      parser: tsParser,
+      ecmaVersion: 6,
+      sourceType: 'commonjs'
+    },
+
+    rules: {
+      'prettier/prettier': 0,
+      'no-param-reassign': 0,
+      'no-return-assign': [2, 'except-parens']
+    }
+  }
+];
